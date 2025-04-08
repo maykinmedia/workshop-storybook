c744e52b433be4158274c1a0ba64881818220552
Sergei Maertens <sergei@maykinmedia.nl>
2025-04-08 16:18:55 +0200

    :tada: Set up Storybook boilerplate and intro

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  core: {
+    disableTelemetry: true,
+    disableWhatsNewNotifications: true,
+  },
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-essentials",
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
